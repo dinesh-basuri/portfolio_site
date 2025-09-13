5932a867c9d4fcbcbe9a953f7391aa5ca8ae43f9
dinesh-basuri <dineshkumar.basuri@gmail.com>
2025-09-13 20:06:52 +0530

    lklk

--- app/components/helper/scroll-to-top.jsx
+++ app/components/helper/scroll-to-top.jsx
@@ -11,23 +11,23 @@ const ScrollToTop = () => {
   const [btnCls, setBtnCls] = useState(DEFAULT_BTN_CLS);
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (typeof window !== "undefined" && typeof document !== "undefined") {
-        console.log('scroll')
-        if (window.scrollY > SCROLL_THRESHOLD) {
-          setBtnCls(DEFAULT_BTN_CLS.replace(" hidden", ""));
-        } else {
-          setBtnCls(DEFAULT_BTN_CLS + " hidden");
-        }
-      }
-      window.addEventListener("scroll", handleScroll, { passive: true });
-      return () => {
-        window.removeEventListener("scroll", handleScroll, { passive: true });
-      };
-    };
-  }, []);
+  //   const handleScroll = () => {
+  //     if (typeof window !== "undefined" && typeof document !== "undefined") {
+  //       console.log('scroll')
+  //       if (window.scrollY > SCROLL_THRESHOLD) {
+  //         setBtnCls(DEFAULT_BTN_CLS.replace(" hidden", ""));
+  //       } else {
+  //         setBtnCls(DEFAULT_BTN_CLS + " hidden");
+  //       }
+  //     }
+  //     window.addEventListener("scroll", handleScroll, { passive: true });
+  //     return () => {
+  //       window.removeEventListener("scroll", handleScroll, { passive: true });
+  //     };
+  //   };
+  // }, []);
 
-  const onClickBtn = () => window.scrollTo({ top: 0, behavior: "smooth" });
+  // const onClickBtn = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
   return (
     <button className={btnCls} onClick={onClickBtn}>
